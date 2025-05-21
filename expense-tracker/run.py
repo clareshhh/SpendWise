c215e48cfd1b001df30ad2b1591365fac80ee8fc
shemcngy <shemcaangay123@gmail.com>
2025-05-21 08:12:32 +0800

    Delete expense-tracker directory

--- expense-tracker/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    print("Starting Expense Tracker server...")
-    print("Open http://localhost:5000 in your browser")
-    app.run(debug=True, host='0.0.0.0', port=5000)
